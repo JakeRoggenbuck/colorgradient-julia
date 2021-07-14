0a5e045db93655a258c2084c0b0b38c89a060085
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-07-14 15:11:02 -0700

    Add comments, change names and test neighbors

--- main.jl
+++ main.jl
@@ -3,10 +3,15 @@ struct Point
     y::Int
 end
 
+# Find the slope of two point
 find_slope(p1, p2) = (p2.y - p1.y) / (p2.x - p1.x)
-part(x) = round(x), ceil(x)
 
+# Find the closest whole numbers on both sides of a x
+neighbors(x) = round(x), ceil(x)
+
+# Find the predicted value of y given a value x
 function find_y(x, known_x)
+    # Check if the given value is exactly one in the known list
     if round(x) == x
         return known_x[x]
     end
@@ -15,4 +20,5 @@ end
 p1 = Point(2, 3)
 p2 = Point(5, 6)
 
-print(find_slope(p1, p2))
+println(find_slope(p1, p2))
+println(neighbors(4.5))
